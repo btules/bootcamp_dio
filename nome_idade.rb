8ed60178146e2aafa5a147eae242a420e069751f
Barbara Tules <barbaratules@gmail.com>
2023-03-09 12:07:35 -0300

    primeiro desafio bootcamp

---
+++ nome_idade.rb
@@ -0,0 +1,11 @@
+#apresenta nome e idade
+
+puts "Qual seu nome?"
+nome = gets.chomp.capitalize
+puts "Qual sua idade?"
+idade = gets.chomp 
+
+puts "Bem vindo(a) #{nome}"
+puts "Você tem #{idade} anos"
+puts "Que legal!!"
+
