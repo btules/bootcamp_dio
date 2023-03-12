5e53ec28b0fa2fccb33d2c66a715df842a78e23e
Barbara Tules <barbaratules@gmail.com>
2023-03-12 18:01:28 -0300

    ultimo desafio bootcamp dio

---
+++ web.rb
@@ -0,0 +1,12 @@
+require 'net/http'
+require 'nokogiri'
+
+https = Net::HTTP.new('example.com', 443)
+https.use_ssl = true
+
+response = https.get('/')
+
+doc = Nokogiri::HTML(response.body)
+paragrafo = doc.at('p')
+
+puts paragrafo.content
